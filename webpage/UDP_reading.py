da6071f4cfb6afd1496d4fd9eab3e607ebd3c268
C2-Z <carloo.sz1130@gmail.com>
2024-09-07 23:14:37 -0500

    db fixed

--- webpage/UDP_reading.py
+++ webpage/UDP_reading.py
@@ -1,13 +1,11 @@
 import socket
 import pymysql
-from datetime import datetime
 
-# Configuración del servidor UDP
-UDP_IP = "0.0.0.0"  # Escuchar en todas las interfaces de red
+
+UDP_IP = "0.0.0.0" 
 UDP_PORT = 5055
-BUFFER_SIZE = 1024  # Tamaño del buffer
+BUFFER_SIZE = 1024 
 
-# Conexión a la base de datos MySQL
 db = pymysql.connect(
     host="localhost",
     user="isa22",
@@ -17,23 +15,19 @@ db = pymysql.connect(
 
 cursor = db.cursor()
 
-# Configuración del socket UDP
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
 print(f"Escuchando en {UDP_IP}:{UDP_PORT}")
 
 while True:
-    # Recibir datos desde el puerto UDP
     data, addr = sock.recvfrom(BUFFER_SIZE)
     print(f"Datos recibidos: {data.decode()} desde {addr}")
 
     try:
-        # Asumiendo que los datos recibidos están en el formato "latitude,longitude"
-        latitude, longitude = map(float, data.decode().split(','))
-
-        # Obtener el timestamp actual
-        timestamp = datetime.now()
+        latitude, longitude, timestamp = data.decode().split(',')
+        latitude = float(latitude)
+        longitude = float(longitude)
 
         # Consulta SQL para actualizar la tabla coordinates
         sql = "INSERT INTO coordinates (latitude, longitude, timestamp) VALUES (%s, %s, %s)"
@@ -46,8 +40,7 @@ while True:
 
     except Exception as e:
         print(f"Error al procesar los datos: {e}")
-        db.rollback()  # Revertir en caso de error
-
-# Cerrar la conexión al finalizar (esto no se alcanzará en un bucle infinito)
+        db.rollback() 
+        
 cursor.close()
 db.close()
