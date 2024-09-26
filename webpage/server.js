016f0fd8f3d7f30cfdfc8cb51fdb0316630d9bd0
bellaom <io404194@gmail.com>
2024-09-26 15:40:40 -0500

    1

--- webpage/server.js
+++ webpage/server.js
@@ -53,6 +53,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/historical', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'historical.html'));
+});
+
 app.get('/api_key', (req, res) => {
     res.json({ key: process.env.GOOGLE_MAPS_API_KEY });
 });
